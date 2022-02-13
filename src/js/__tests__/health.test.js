60fa5adcf946733d723b7e7372144a3ccbceae1b
Roman <localhost>
2022-02-13 12:51:35 +0700

    new version

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -1,6 +1,16 @@
 import getHealthStatus from '../app';
 
-test('should be wounded', () => {
+test('should return wounded', () => {
   const result = getHealthStatus({ name: 'Маг', health: 41 });
   expect(result).toEqual('wounded');
+});
+
+test('should return critical', () => {
+  const result = getHealthStatus({ name: 'Маг', health: 12 });
+  expect(result).toEqual('critical');
+});
+
+test('should return healthy', () => {
+  const result = getHealthStatus({ name: 'Маг', health: 77 });
+  expect(result).toEqual('healthy');
 });
